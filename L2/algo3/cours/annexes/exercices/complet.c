3706bf5c6c61b6b62a57bb13d5edd130b64d152f
Antoine de Roquemaurel <satenske@Ace>
2012-11-13 09:56:52 +0100

    Minitoc algo
    Former-commit-id: bad51d1eda45e075e184ea142a500ccc5d3d44c2

---
+++ L2/algo3/cours/annexes/exercices/complet.c
@@ -0,0 +1,20 @@
+/* PE : (N > 0) $\wedge$ T(0..N, $\leq$) */
+int i  = 1;
+int nb = 1;
+int t  = 1;
+
+/*  INV  */
+while(i != N) {
+	/*  INV $\wedge$ C*/
+}
+
+/* On remplace une constante par une variable ainsi on a $\neg$C $\wedge$ INV
+ * PS : (i = N) $\wedge$ nb $\geq$ 1 $\wedge$ nb = $\nu$I : 0 $\leq$ I $\leq$ i - t 
+ * $\wedge$ T[I] = T[I+t-1] $\wedge$ 
+ * $\forall$ J : O $\leq$ J $\leq$ i-t-1 $\rightarrow$ T[J] $\neq$ T[J+t] 
+ */
+
+/* PS : nb $\geq$ 1 $\wedge$ nb = $\nu$I : 0 $\leq$ I $\leq$ N - t $\wedge$ T[I] = T[I+t-1] $\wedge$ 
+ * $\forall$ J : O $\leq$ J $\leq$ N-t-1 $\rightarrow$ T[J] $\neq$ T[J+t] 
+ */
+
