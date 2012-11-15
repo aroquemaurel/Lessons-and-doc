85e9c68ada03b90b85ede34e8e86a9f4352e9218
Antoine de Roquemaurel <satenske@luffy>
2012-11-15 14:10:45 +0100

    TP Algo
    Former-commit-id: 4f52e72bb0a09c97768494d613a37c68c1ddbbf3

---
+++ L2/algo3/TP/TP5/element.c
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  element.c
+ *
+ *    Description:  Implémentation des fonctions sur les éléments
+ *
+ *        Version:  1.0
+ *        Created:  06/11/2012 18:14:00
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Antoine de Roquemaurel (), 
+ *
+ * =====================================================================================
+ */
+#include "element.h"
+
+void elem_display(const Element pElem) {
+	printf("nb : %d", pElem.nb);
+}
+void elem_saisir(Element* pElem) {
+	printf("Entrer nb: ");
+	scanf("%d", &(pElem->nb));
+}
+
+void elem_affect(Element* pElem1, const Element pElem2) {
+	pElem1->nb = pElem2.nb;
+}
+
+void elem_initialize(Element* pElem) {
+	pElem->nb = 0;
+}
