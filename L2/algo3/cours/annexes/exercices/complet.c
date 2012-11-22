4a72ed78873059af3d8c21207dc7999f8e48da9a
Antoine de Roquemaurel <satenske@Ace>
2012-11-23 00:00:58 +0100

    Cours algo
    Former-commit-id: c6711538425e9ed847c76a98f822294b11dfd5fe

--- L2/algo3/cours/annexes/exercices/complet.c
+++ L2/algo3/cours/annexes/exercices/complet.c
@@ -1,20 +1,31 @@
 /* PE : (N > 0) $\wedge$ T(0..N, $\leq$) */
 int i  = 1;
 int nb = 1;
-int t  = 1;
+int p  = 1;
 
-/*  INV  */
+/*  INV= 1 $\leq$ p$\leq$ i $\wedge$ nb $\geq$ 1 $\wedge$ nb = $\nu$I : 0 $\leq$ I $\leq$ i - p 
+ * $\wedge$ T[I] = T[I+p-1] $\wedge$ 
+ * $\forall$ J : O $\leq$ J $\leq$ i-p-1 $\rightarrow$ T[J] $\neq$ T[J+p] 
+ */
 while(i != N) {
 	/*  INV $\wedge$ C*/
+	if (T[i] == T[i-p]) {
+		np = 1;
+		++p;
+	} else if(T[i] == T[i-p+1]) {
+		++np;
+	}
+	++i;
+	/*  INV  */
 }
 
 /* On remplace une constante par une variable ainsi on a $\neg$C $\wedge$ INV
- * PS : (i = N) $\wedge$ nb $\geq$ 1 $\wedge$ nb = $\nu$I : 0 $\leq$ I $\leq$ i - t 
- * $\wedge$ T[I] = T[I+t-1] $\wedge$ 
- * $\forall$ J : O $\leq$ J $\leq$ i-t-1 $\rightarrow$ T[J] $\neq$ T[J+t] 
+ * PS : (i = N) $\wedge$ nb $\geq$ 1 $\wedge$ nb = $\nu$I : 0 $\leq$ I $\leq$ i - p 
+ * $\wedge$ T[I] = T[I+p-1] $\wedge$ 
+ * $\forall$ J : O $\leq$ J $\leq$ i-p-1 $\rightarrow$ T[J] $\neq$ T[J+p] 
  */
 
-/* PS : nb $\geq$ 1 $\wedge$ nb = $\nu$I : 0 $\leq$ I $\leq$ N - t $\wedge$ T[I] = T[I+t-1] $\wedge$ 
- * $\forall$ J : O $\leq$ J $\leq$ N-t-1 $\rightarrow$ T[J] $\neq$ T[J+t] 
+/* PS : 1 $\leq$ p$\leq$ i $\wedge$ nb $\geq$ 1 $\wedge$ nb = $\nu$I : 0 $\leq$ I $\leq$ N - p $\wedge$ T[I] = T[I+p-1] $\wedge$ 
+ * $\forall$ J : O $\leq$ J $\leq$ N-p-1 $\rightarrow$ T[J] $\neq$ T[J+p] 
  */
 
