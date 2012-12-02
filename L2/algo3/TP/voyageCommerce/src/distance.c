8dbcb400ef4e0134d509c0c0daa40e75e254b61d
Antoine de Roquemaurel <satenske@luffy>
2012-12-02 21:08:55 +0100

    Voyageur de commerce

---
+++ L2/algo3/TP/voyageCommerce/src/distance.c
@@ -0,0 +1,52 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  distance.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/12/2012 20:33:39
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Antoine de Roquemaurel (), 
+ *
+ * =====================================================================================
+ */
+#include "distance.h"
+
+void distance_new(Distance* pDistance, Town* pFirstTown, Town* pSecondTown) {
+	pDistance->firstTown = *pFirstTown;
+	pDistance->secondTown = *pSecondTown;
+	pDistance->distance = distance_calculDistance(*pFirstTown, *pSecondTown);
+}
+
+double distance_calculDistance(const Town pTown1, const Town pTown2) {
+	double x1=pTown1.x, x2=pTown2.x;
+	double y1=pTown1.y, y2=pTown2.y;
+	return (sqrt(pow((x1 > x2) ? x1 - x2 : x2 - x1, 2) + 
+				 pow((y1 > y2) ? y1 - y2 : y2 - y1, 2)
+			)); //Pythagore
+}
+Distance distance_searchDistance(Distance* pDistances, const int pFirst, const int pSecond) {
+	int i = util_sum(0,pFirst);
+	int search = 0;
+
+	while(search != pSecond) {
+		search = pDistances[i++].secondTown.id;	
+	}
+
+	return (pDistances[i]);
+}
+double distance(Distance* pDistances, int i, int j) {
+	Distance distance;
+	if(j > i) {
+		util_swap(&i, &j);
+	} else if(j == i) {
+		return 0;
+	}
+	distance = distance_searchDistance(pDistances, i, j);
+
+	return (distance.distance);
+}
