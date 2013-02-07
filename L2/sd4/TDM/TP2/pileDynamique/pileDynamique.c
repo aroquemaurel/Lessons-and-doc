29afb32e041e55f286c659325887ac9d81850c2c
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2013-02-07 21:22:31 +0100

    Cours de gestion de projet
    Former-commit-id: 63ec10a0e20b8fac5c285085cddddcdf85863968

--- L2/sd4/TDM/TP2/pileDynamique/pileDynamique.c
+++ L2/sd4/TDM/TP2/pileDynamique/pileDynamique.c
@@ -1,52 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+typedef int Element;
 typedef struct etCel {
-	Element val;
-	struct etCel* suiv;
+	Element value;
+	struct etCel* next;
 } Cel;
+#include "pileDynamique.h"
 
-typedef cel* Pile;
 
-Pile creer() {
-	return NULL;
-}
+int main(int argc, char** argv) {
+	Pile p = pile_init();
+	p = pile_saisir(p);
+	pile_afficher(p);
+	printf("\n");
+	pile_depiler(&p);
+	pile_depiler(&p);
+	pile_empiler(&p, 42);
 
-Pile empiler(Pile p, Element x) {
-	Pile pAux;
-	pAux = (pile)malloc(sizeof(Cel));
-	assert(pAux != NULL);
-	pAux->val = x;
-	pAux->suiv = p;
-
-	return (pAux);
-}
-
-int estVide(Pile p) {
-	return (p == NULL);
-}
-
-Pile depiler(Pile p) {
-	Pile pAux = NULL;
-	if(p != NULL) {
-		pAux = p->suivant;	
-		free(p);
-	}
-
-	return pAux;
-}
-
-Element sommet(Pile p) {
-	return (p->val);
-}
-
-int appartient(Pile p, Element x) {
-	if(estVide(p))
-		return 0;
-
-	while(!estVide(p)) {
-		if(p->suiv == x)
-			return 1;
-
-		p = p->suiv;
-	}
-
+	pile_afficher(p);
 	return 0;
+}
+
+Pile pile_init() {
+	return (NULL);	
+}
+
+void pile_afficher(Pile pStack) {
+	while(!pile_estVide(pStack)) {
+		printf("%d ", pStack->value);
+		pStack = pStack->next;
+	}
+}
+
+bool pile_estVide(const Pile pStack) {
+	return (pStack == NULL);
+}
+
+bool pile_pleine(const Pile pStack) {
+	return false; // dynamique donc jamais pleine
+}
+
+void pile_empiler(Pile* pStack, Element pElem) {
+	Pile buffStack;
+	buffStack = (Pile)malloc(sizeof(Cel));
+
+	assert(buffStack != NULL);
+	buffStack->value = pElem;
+	buffStack->next= *pStack;
+	*pStack = buffStack;
+}
+
+void pile_empilerPlusieursElements(Pile* pStack, Element* pElem, int nbElem) {
+	int i;
+	for(i=0; i < nbElem ; ++i) {
+		pile_empiler(pStack, pElem[i]);
+	}
+}
+Element pile_depiler(Pile* pStack) {
+	Pile buffStack= NULL;
+
+	if(!pile_estVide(*pStack)) {
+		buffStack = (*pStack)->next;;
+		free(*pStack);
+	}
+	*pStack = buffStack;	
+
+	return (buffStack->value);
+}
+
+Pile pile_saisir() {
+	Pile stack = pile_init();
+	Element elem = 0;
+	scanf(" %d", &elem);
+	while(elem != -1) {
+		pile_empiler(&stack, elem);
+		scanf(" %d", &elem);
+	}
+
+	return stack;
+}
+
+Element pile_sommet(Pile pStack) { 
+	return (pStack->value);
 }
