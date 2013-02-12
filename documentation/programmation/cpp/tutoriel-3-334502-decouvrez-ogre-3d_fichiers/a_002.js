6395d7299618b7ff6e53c91464d4bb7b8ff221b5
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2013-02-12 13:44:55 +0100

    Suppresion documentation/ suite à renommage
    Former-commit-id: f9cd1a38e13892a756896ca62e25bb6918039400

--- documentation/programmation/cpp/tutoriel-3-334502-decouvrez-ogre-3d_fichiers/a_002.js
+++
@@ -1,5 +0,0 @@
-if (screen.width && screen.height) {var jsinfocall=new Image;jsinfocall.src='http://www4.smartadserver.com/a/track/jsinfo.asp?sw='+screen.width+'&sh='+screen.height;}
-
-sas_noad = true;
-
-
