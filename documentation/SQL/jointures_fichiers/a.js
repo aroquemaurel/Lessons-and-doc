6de055a422752b4bc8fe7523a055a0f80b471920
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2013-02-12 13:44:55 +0100

    Suppresion documentation/ suite à renommage

--- documentation/SQL/jointures_fichiers/a.js
+++
@@ -1,4 +0,0 @@
-
-sas_noad = true;
-
-
--- documentation/programmation/java/java_files/pub.js(1).php
+++
@@ -1,3 +0,0 @@
-if (document.getElementById('pave_pub_tuto')) { document.getElementById('pave_pub_tuto').style.display = 'block'; }
-document.write('<iframe src="http://rcm-fr.amazon.fr/e/cm?t=lesiduze-21&o=8&p=12&l=st1&mode=books-fr&search=java%20programmation&fc1=000000&lt1=&lc1=3366FF&bg1=FFFFFF&f=ifr" marginwidth="0" marginheight="0" width="300" height="250" border="0" frameborder="0" style="border:none;" scrolling="no"></iframe>');
-document.write('');
