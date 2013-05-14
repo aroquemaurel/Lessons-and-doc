a5feea8ec5de15981f0f064e6b4fada184edf7ea
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2013-05-14 17:18:25 +0200

    structures de données

---
+++ L2/sd4/DM/DM2/couleur.h
@@ -0,0 +1,7 @@
+#ifndef __COULEUR
+#define __COULEUR
+
+typedef enum { ROUGE, NOIR } Couleur;
+
+#endif
+
---
+++ L2/sd4/DM/DM2/main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include "arn.h"
+
+void* compare_int(void* premierOperande, void* secondeOperande);
+void afficher_int(void* valeur, Couleur couleur);
+char* affiche_noeud(void* valeur, Couleur couleur);
+void ecrireDotEtapes(ARN t, char* repertoire);
+
+
+int main(void) {
+	int i=0;
+	char *repertoire = "arbreRougeNoir_dot"; // le repertoire ou seront entreposés les .dot et .png
+	char commandeDot[256] = "./scriptDot.sh ";
+	strcat(commandeDot, repertoire);
+	strcat(commandeDot, "/fichier");
+
+	ARN t = arn_creer();
+	arn_inserer(t, (void*) 18, compare_int);
+	ecrireDotEtapes(t, repertoire);
+	arn_inserer(t, (void*) 98, compare_int);
+	ecrireDotEtapes(t, repertoire);
+	arn_inserer(t, (void*) 51, compare_int);
+	ecrireDotEtapes(t, repertoire);
+	arn_inserer(t, (void*) 70, compare_int);
+	ecrireDotEtapes(t, repertoire);
+	arn_inserer(t, (void*) 62, compare_int);
+	ecrireDotEtapes(t, repertoire);
+	arn_inserer(t, (void*) 42, compare_int);
+	ecrireDotEtapes(t, repertoire);
+	arn_supprimer(t, (void*) 98, compare_int);
+	ecrireDotEtapes(t, repertoire);
+	arn_supprimer(t, (void*) 51, compare_int);
+	ecrireDotEtapes(t, repertoire);
+	arn_inserer(t, (void*) 69, compare_int);
+	ecrireDotEtapes(t, repertoire);
+	arn_inserer(t, (void*) 132, compare_int);
+	ecrireDotEtapes(t, repertoire);
+	arn_supprimer(t, (void*) 18, compare_int);
+	ecrireDotEtapes(t, repertoire);
+	arn_inserer(t, (void*) 1, compare_int);
+	ecrireDotEtapes(t, repertoire);
+
+	system(commandeDot); // on lance un script shell qui génère tous les .png et un .gif
+	return 0;
+}
+
+void ecrireDotEtapes(ARN t, char* repertoire) {
+	static int i = 1;
+	char fileName[256];
+	sprintf(fileName, "%s/fichier%d.dot", repertoire, i);
+	FILE* fichier = fopen(fileName, "w+");
+	if (fichier == NULL) {
+		perror("Erreur d'ouverture du fichier");
+	}
+	arn_ecrireFichierDot(t, affiche_noeud, fichier);
+	fclose(fichier);
+	++i;
+}
+
+char* affiche_noeud(void* valeur, Couleur couleur) {
+	static char ret[256];
+	sprintf(ret,"%d", (int)valeur);
+	return ret; 
+}
+
+void afficher_int(void* valeur, Couleur couleur) {
+	printf("%s%d ", (couleur == ROUGE) ? "\033[01;31m" : "\033[0m", valeur);
+}
+
+void* compare_int(void* premierOperande, void* secondeOperande) {
+	int op1 = (int)premierOperande;
+	int op2 = (int)secondeOperande;
+	int retour;
+
+	if(op1 < op2) {
+		retour = -1;
+	} else if (op1 > op2) {
+		retour = 1;
+	} else {
+		retour = 0;
+	}
+
+	return retour;
+}
+
