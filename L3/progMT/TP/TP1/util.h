82b737cc08686ebbc5308b949c1806149b0d6327
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2013-10-18 10:22:42 +0200

    tp progMT
    Former-commit-id: c9bb1a0deb32d30c659ce313bc074921462ebacb

---
+++ L3/progMT/TP/TP1/util.h
@@ -0,0 +1,11 @@
+#ifndef __UTIL
+#define __UTIL
+
+char** str_split(char* a_str, const char a_delim);
+char *trim (char *str);
+char *remove_newline(char *s);
+char** trimTab(char** s, int nb);
+
+#endif
+
+
