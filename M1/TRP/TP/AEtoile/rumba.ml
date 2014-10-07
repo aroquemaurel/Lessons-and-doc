31eaf20a6a4a81e7b2d4b840b8a39523d7b9a8df
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2014-10-07 09:50:16 +0200

    [TRP] Add rumba
    Former-commit-id: 17b487286af86fac84653a90e67a0cb6346a5566

---
+++ M1/TRP/TP/AEtoile/rumba.ml
@@ -0,0 +1,10 @@
+(*
+    Rumba des chiffres
+
+    - Choisi d'une représentatino Interne
+    - Petite fonction d'affichage
+    - opPos
+        (deplacer etat picDepart picArrive) -> nouvel etat
+    - Trouver un heuristique (h) : s'aider des idées du taquin.
+        Combien de cube ne sont pas à leur place... heuristique bête, mais qui donne une première info
+*)
