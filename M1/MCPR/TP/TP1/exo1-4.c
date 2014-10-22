e0ab46c84d0fd4de0b140af8fb639d343bd7c910
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2014-10-22 10:33:36 +0200

    MCPR create v4

---
+++ M1/MCPR/TP/TP1/exo1-4.c
@@ -0,0 +1,66 @@
+/*  TP1 -- Exo1 -- variante 3 */
+
+/* 
+ * Modifier le code précédent pour utiliser maintenant un segment de mémoire
+ * partagée pour implanter ce compteur.
+ */
+#define _XOPEN_SOURCE
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int* cpt;
+
+void treatmentIncrement(const int nbIncrement);
+void treatmentDecrement(const int nbDecrement);
+
+int main(int argc, char** argv) {
+	int memId;
+	key_t KEY = ftok("main.c", 'r');
+	if(argc <= 1) {
+		perror("Arguments errors");
+		exit(1);
+	}
+
+
+	if ((memId = shmget(KEY, sizeof(int), 0666|IPC_CREAT)) < 0) {
+		perror("shmget");											
+		exit(1);
+	}
+	if ((cpt = shmat(memId, NULL, 0)) == (void*) -1) {
+		perror("shmat");					
+		exit(1);
+	}
+
+	switch(fork()) {
+		case 0:
+			treatmentIncrement(atoi(argv[1]));
+			break;
+		default:
+			treatmentDecrement(atoi(argv[1]));
+			break;
+	}
+	shmdt(cpt);
+	wait(NULL);
+	shmctl(memId, IPC_RMID, NULL);
+}
+
+void treatmentIncrement(const int nbIncrement) {
+	for(int i = 0 ; i < nbIncrement; ++i) {
+		printf("\tIncrement: %d\n",++(*cpt));
+	}
+
+}
+
+void treatmentDecrement(const int nbDecrement) {
+	for(int i = 0 ; i < nbDecrement; ++i) {
+		printf("Decrement: %d\n",--(*cpt));
+	}
+
+}
